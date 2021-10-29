5844003b2f3b5ec5e5e525b64da52c0726373fac
Lucinda Verplanck <75956292+Pixeldeerest@users.noreply.github.com>
2021-10-28 22:05:47 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import wikipedia
+import json 
+from urllib.parse import urlparse 
+from flask import Flask, url_for, request
+from wikipedia.wikipedia import WikipediaPage
+
+app = Flask(__name__) 
+
+if __name__ == "main":
+    app.config['SERVER_NAME'] = 'wiki-search.com'
+    app.run()
+
+@app.route("/", methods=['GET'])
+def wikisearch_index():
+    url = urlparse("http://ordinary.wiki-search.com")
+    searchTerm = url.hostname.split('.')[0]
+
+    wikiResults = {"links":[]}
+    try:
+        wikiPage = wikipedia.page(searchTerm)
+        wikiURL = wikiPage.url
+        wikiResults["links"].append(wikiURL)
+    except wikipedia.exceptions.DisambiguationError as e:
+        for option in e.options:
+            try:
+                wikiPage = wikipedia.page(option)
+                wikiURL = wikiPage.url
+            except wikipedia.exceptions.DisambiguationError as e:
+                continue
+            wikiResults["links"].append(wikiURL)
+    result = json.dumps(wikiResults)
+    return result
+
